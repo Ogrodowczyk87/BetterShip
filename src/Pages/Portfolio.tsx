b77cf32e8dac99fc196cb14b6aed5ad96df31372
Rafal <rafal@Zastrzyk.Zastzyk>
2025-10-26 21:06:42 +0000

    bg

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -3,9 +3,9 @@ import { PortfolioCards } from "../components/PortfolioCards";
 
 export default function Portfolio() {
   return (
-    <section id="portfolio" className="py-4">
-      <div className="max-w-6xl mx-auto px-6">
-        <h2 className="text-4xl font-semibold mb-8 text-left">Portfolio</h2>
+    <section id="portfolio" className="bg-black py-4">
+      <div className="max-w-6xl mx-auto px-3">
+        <h2 className="text-5xl font-semibold mb-3 text-left">Portfolio</h2>
         <div>
           <PortfolioCards />
         </div>
