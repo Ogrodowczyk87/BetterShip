354b1ccfd5bf2948e8914ecfe915c8ff85f20412
Rafal <rafal@Zastrzyk.Zastzyk>
2026-01-18 10:47:44 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Home from "./Pages/Home";
-import LightRays from "./components/LightRays";
+// import LightRays from "./components/LightRays";
 
 export default function App() {
   return (
     <div className="relative overflow-hidden py-5 bg-white text-slate-900">
       <div className="pointer-events-none absolute inset-0 z-0">
-        <LightRays
+        {/* <LightRays
           className="w-full h-full opacity-15 mix-blend-multiply"
           raysOrigin="top-center"
           raysColor="#bfdbfe"
@@ -18,7 +18,7 @@ export default function App() {
           noiseAmount={0.0}
           distortion={0.03}
           fadeDistance={2.6}
-        />
+        /> */}
       </div>
       <Home />
     </div>
